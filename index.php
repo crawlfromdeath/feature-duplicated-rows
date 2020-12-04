97cbcc2d81aab9e14c298daace4d5e30e2025c61
Miltos Konstantoudakis <miltoskonstantoudakis@gmail.com>
2020-12-04 21:50:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	die();
+
 	$connection = mysqli_connect("db-mysql-sfo3-52037-do-user-4596315-0.b.db.ondigitalocean.com","doadmin","qmdvp61fnm8azm2u","defaultdb", "25060");
 
 	if (mysqli_connect_errno()) {
